5bd9ba0f36451fc328f3338bf458edab8aa04408
Omni <rafazel@outlook.com>
2020-08-24 21:54:30 -0300

    exe7 realizado

---
+++ exe7.js
@@ -0,0 +1,19 @@
+chefe()
+
+function exe7 (){
+exe7()
+}
+function exe7(){
+    var soma=0
+    var numero =Number(prompt("informe um número"))
+    do {
+        conta++ //soma 1
+        
+        soma= soma+numero //acumulador
+
+        numero= Number(prompt("informe outro número. Digite número negativo para encerrar"))
+    }
+    while (numero>= 0)
+    
+    console.log(`A média dos números informados é ${somaconta}`)
+}
